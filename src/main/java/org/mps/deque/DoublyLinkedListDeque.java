5aff933869c1ea7b7df19583233e35a49bbd0541
Lucia <luciagm639@gmail.com>
2023-03-07 11:45:32 +0100

    Finished DoublyLinkedListDeque

--- src/main/java/org/mps/deque/DoublyLinkedListDeque.java
+++ src/main/java/org/mps/deque/DoublyLinkedListDeque.java
@@ -14,41 +14,72 @@ public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
 
     @Override
     public void prepend(T value) {
-        DequeNode<T> aux = first;
-        first = new DequeNode(aux, null, aux);
-        aux.setPrevious(first);
+        if (size() == 0) {
+            first = new DequeNode(value, null, null);
+            last = first;
+        } else {
+            DequeNode<T> aux = first;
+            first = new DequeNode(value, null, aux);
+            aux.setPrevious(first);
+        }
+        size++;
     }
 
     @Override
     public void append(T value) {
-        // TODO
+        if (size() == 0) {
+            first = new DequeNode(value, null, null);
+            last = first;
+        } else {
+            DequeNode<T> aux = last;
+            last = new DequeNode(value, aux, null);
+            aux.setNext(last);
+        }
+        size++;
     }
 
     @Override
     public void deleteFirst() {
-        // TODO
+        if (size() == 0) {
+            throw new DoubleEndedQueueException("Attempt to delete the first element from an empty DoubleLinkedListDeque");
+        }
+        DequeNode<T> aux = first;
+        first = aux.getNext();
+        first.setPrevious(null);
+        aux.setNext(null);
+        size--;
     }
 
     @Override
     public void deleteLast() {
-        // TODO
+        if (size() == 0) {
+            throw new DoubleEndedQueueException("Attempt to delete the last element from an empty DoubleLinkedListDeque");
+        }
+        DequeNode<T> aux = last;
+        last = aux.getPrevious();
+        last.setNext(null);
+        aux.setPrevious(null);
+        size--;
     }
 
     @Override
     public T first() {
-        // TODO
-        return null;
+        if (size() == 0) {
+            throw new DoubleEndedQueueException("Attempt to get the first element from an empty DoubleLinkedListDeque");
+        }
+        return first.getItem();
     }
 
     @Override
     public T last() {
-        // TODO
-        return null;
+        if (size() == 0) {
+            throw new DoubleEndedQueueException("Attempt to get the last element from an empty DoubleLinkedListDeque");
+        }
+        return last.getItem();
     }
 
     @Override
     public int size() {
-        // TODO
-        return 0;
+        return size;
     }
 }
