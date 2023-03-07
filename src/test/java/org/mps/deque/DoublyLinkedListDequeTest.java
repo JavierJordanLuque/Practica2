e5250499281e134bddd77c4fc50f34da8a9fc0a1
Javier Jordán Luque <javierjordanluque@gmail.com>
2023-03-07 12:33:45 +0100

    Modified Test

--- src/test/java/org/mps/deque/DoublyLinkedListDequeTest.java
+++ src/test/java/org/mps/deque/DoublyLinkedListDequeTest.java
@@ -1,6 +1,5 @@
 package org.mps.deque;
 
-<<<<<<< HEAD
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,8 +25,6 @@ import static org.junit.jupiter.api.Assertions.*;
     16. Size of a non-empty DoublyLinkedListDeque with two elements {12, 13} -> Returns 2
      */
 
-=======
->>>>>>> b5c718ab8d4a10d308a82f587ecd3ac4abb68131
 class DoublyLinkedListDequeTest {
 
     DoublyLinkedListDeque<Integer> list;
